9e862ac73d24c5bd83b2aac2b2b797acee8a6e45
Christian Hovy <hovy@informatik.uni-hamburg.de>
2017-12-09 00:25:45 +0100

    Can load different config files now

--- FortranTestGenerator.py
+++ FortranTestGenerator.py
@@ -8,19 +8,21 @@ Created on 05.02.2016
 
 import argparse;
 
-from config_fortrantestgenerator import SOURCE_DIR, SOURCE_FILES, TEMPLATE_DIR, BACKUP_SUFFIX, TEST_SOURCE_DIR, TEST_DATA_BASE_DIR, GRAPH_BUILDER, IGNORE_GLOBALS_FROM_MODULES, IGNORE_DERIVED_TYPES, EXCLUDE_MODULES, FTG_PREFIX
-
-from source import SubroutineFullName
+from source import SubroutineFullName, SourceFiles
 from capture import CaptureCodeGenerator
 from replay import ReplayCodeGenerator
 from backup import BackupFileFinder
 from combined import CombinedCodeGenerator
+from ftgconfigurator import loadFortranTestGeneratorConfiguration
+from fcgconfigurator import loadFortranCallGraphConfiguration
+from assembler import FromAssemblerCallGraphBuilder
+from treecache import CachedAssemblerCallGraphBuilder
 
 def parseArguments(argParser):
-    
     argParser.add_argument('-b', '--restore', action="store_true", help='Restore Backup Files')
     argParser.add_argument('-c', '--capture', action="store_true", help='Generate Capture Code')
     argParser.add_argument('-r', '--replay', action="store_true", help='Generate Replay Code')
+    argParser.add_argument('-cf', '--configFile', type=str, help='Import configuration from this file.');
     argParser.add_argument('module', nargs='?', default=None);
     argParser.add_argument('subroutine', nargs='?', default=None);
     return argParser.parse_args();
@@ -28,11 +30,30 @@ def parseArguments(argParser):
 def main():
     argParser = argparse.ArgumentParser(description='Generate test code.');
     args = parseArguments(argParser);
-
     if not (args.restore or args.capture or args.replay):
         argParser.error('No action requested, add -b/--restore and/or -c/--capture and/or -r/--replay')
 
-    backupFinder = BackupFileFinder(SOURCE_DIR, BACKUP_SUFFIX) 
+    config = loadFortranTestGeneratorConfiguration(args.configFile)
+    if config is None:
+        exit(3)
+    config.update(loadFortranCallGraphConfiguration(config['FCG_CONFIG_FILE']))
+    config.update(loadFortranTestGeneratorConfiguration(args.configFile)) # Load again to overwrite variables from FCG config file
+
+    SOURCE_DIR = config['SOURCE_DIR']
+    BACKUP_SUFFIX = config['BACKUP_SUFFIX']
+    FTG_PREFIX = config['FTG_PREFIX']
+    TEMPLATE_DIR = config['TEMPLATE_DIR']
+    TEST_SOURCE_DIR = config['TEST_SOURCE_DIR']
+    TEST_DATA_BASE_DIR = config['TEST_DATA_BASE_DIR']
+    EXCLUDE_MODULES = config['EXCLUDE_MODULES']
+    IGNORE_GLOBALS_FROM_MODULES = config['IGNORE_GLOBALS_FROM_MODULES']
+    IGNORE_DERIVED_TYPES = config['IGNORE_DERIVED_TYPES']
+
+    GRAPH_BUILDER = FromAssemblerCallGraphBuilder(config['ASSEMBLER_DIR'], config['SPECIAL_MODULE_FILES'])
+    if config['CACHE_DIR']:
+        GRAPH_BUILDER = CachedAssemblerCallGraphBuilder(config['CACHE_DIR'], GRAPH_BUILDER)
+    SOURCE_FILES = SourceFiles(config['SOURCE_DIR'], config['SPECIAL_MODULE_FILES'])
+    BACKUP_FINDER = BackupFileFinder(SOURCE_DIR, BACKUP_SUFFIX) 
 
     if args.capture or args.replay:
         moduleName = args.module;
@@ -57,10 +78,10 @@ def main():
         
     if args.restore:
         print 'Restore Backup Files'
-        backupFinder.restore()
+        BACKUP_FINDER.restore()
         SOURCE_FILES.clearCache()
     else: 
-        SOURCE_FILES.setSpecialModuleFiles(backupFinder.extendSpecialModuleFiles(SOURCE_FILES.getSpecialModuleFiles()))
+        SOURCE_FILES.setSpecialModuleFiles(BACKUP_FINDER.extendSpecialModuleFiles(SOURCE_FILES.getSpecialModuleFiles()))
 
     if args.capture or args.replay:
         if args.capture and args.replay:
