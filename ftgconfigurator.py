19f767d9ab95910ca33043988ef4f77d9ae96870
Christian Hovy <hovy@informatik.uni-hamburg.de>
2017-12-12 15:58:40 +0100

    Constants for config keys

--- ftgconfigurator.py
+++ ftgconfigurator.py
@@ -1,5 +1,14 @@
 import os
 import sys
+
+CFG_TEMPLATE_DIR = 'TEMPLATE_DIR'
+CFG_TEST_SOURCE_DIR = 'TEST_SOURCE_DIR'
+CFG_TEST_DATA_BASE_DIR = 'TEST_DATA_BASE_DIR'
+CFG_MODIFY_SOURCE_DIRS = 'MODIFY_SOURCE_DIRS'
+CFG_MODIFY_SOURCE_DIRS_LEGACY = 'MODIFY_SOURCE_DIR'
+CFG_BACKUP_SUFFIX = 'BACKUP_SUFFIX'
+CFG_FTG_PREFIX = 'FTG_PREFIX'
+CFG_FCG_CONFIG_FILE = 'FCG_CONFIG_FILE'
         
 def loadFortranTestGeneratorConfiguration(configFile):
     if configFile:
@@ -38,29 +47,33 @@ def loadFortranTestGeneratorConfiguration(configFile):
             else:
                 sys.path.append(config['FCG_DIR'])
 
-    if 'TEMPLATE_DIR' not in config or not config['TEMPLATE_DIR']:
+    if CFG_TEMPLATE_DIR not in config or not config[CFG_TEMPLATE_DIR]:
         print >> sys.stderr, 'Missing config variable: TEMPLATE_DIR'
         configError = True
 
-    if 'TEST_SOURCE_DIR' not in config or not config['TEST_SOURCE_DIR']:
+    if CFG_TEST_SOURCE_DIR not in config or not config[CFG_TEST_SOURCE_DIR]:
         print >> sys.stderr, 'Missing config variable: TEST_SOURCE_DIR'
         configError = True
 
-    if 'TEST_DATA_BASE_DIR' not in config or not config['TEST_DATA_BASE_DIR']:
+    if CFG_TEST_DATA_BASE_DIR not in config or not config[CFG_TEST_DATA_BASE_DIR]:
         print >> sys.stderr, 'Missing config variable: TEST_DATA_BASE_DIR'
         configError = True
 
-    if 'MODIFY_SOURCE_DIR' not in config or not config['MODIFY_SOURCE_DIR']:
-        config['MODIFY_SOURCE_DIR'] = None
+    if CFG_MODIFY_SOURCE_DIRS not in config and CFG_MODIFY_SOURCE_DIRS_LEGACY in config:
+        config[CFG_MODIFY_SOURCE_DIRS] = config[CFG_MODIFY_SOURCE_DIRS_LEGACY]
+    if CFG_MODIFY_SOURCE_DIRS not in config or not config[CFG_MODIFY_SOURCE_DIRS]:
+        config[CFG_MODIFY_SOURCE_DIRS] = None
+    elif isinstance(config[CFG_MODIFY_SOURCE_DIRS], str):
+        config[CFG_MODIFY_SOURCE_DIRS] = [config[CFG_MODIFY_SOURCE_DIRS]]
         
-    if 'BACKUP_SUFFIX' not in config or not config['BACKUP_SUFFIX']:
-        config['BACKUP_SUFFIX'] = 'ftg-backup'
+    if CFG_BACKUP_SUFFIX not in config or not config[CFG_BACKUP_SUFFIX]:
+        config[CFG_BACKUP_SUFFIX] = 'ftg-backup'
 
-    if 'FTG_PREFIX' not in config or not config['FTG_PREFIX']:
-        config['FTG_PREFIX'] = 'ftg_'
+    if CFG_FTG_PREFIX not in config or not config[CFG_FTG_PREFIX]:
+        config[CFG_FTG_PREFIX] = 'ftg_'
 
-    if 'FCG_CONFIG_FILE' not in config or not config['FCG_CONFIG_FILE']:
-        config['FCG_CONFIG_FILE'] = None
+    if CFG_FCG_CONFIG_FILE not in config or not config[CFG_FCG_CONFIG_FILE]:
+        config[CFG_FCG_CONFIG_FILE] = None
     
     if configError:
         return None
