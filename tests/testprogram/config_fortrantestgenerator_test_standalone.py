816006a1b9d3696df71e02096013787d963abf86
Christian Hovy <hovy@informatik.uni-hamburg.de>
2018-05-12 00:44:01 +0200

    [tests] Fixed path in testprogram configuration

--- tests/testprogram/config_fortrantestgenerator_test_standalone.py
+++ tests/testprogram/config_fortrantestgenerator_test_standalone.py
@@ -4,7 +4,7 @@ FTG_DIR = os.path.dirname(os.path.realpath(__file__))
 FCG_DIR = FTG_DIR + '/../fortrancallgraph'
 TEMPLATE_DIR = FTG_DIR + '/templates/standalone'
 
-TEST_DIR = FTG_DIR + 'tests/testprogram'
+TEST_DIR = FTG_DIR + '/tests/testprogram'
 
 FCG_CONFIG_FILE = TEST_DIR + '/config_fortrancallgraph_test.py'
 TEST_SOURCE_DIR = TEST_DIR + '/replay'
