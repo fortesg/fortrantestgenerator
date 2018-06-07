e540bd5dd7d14b92784e69135a109760fa47501b
Christian Hovy <hovy@informatik.uni-hamburg.de>
2018-06-07 10:59:05 +0200

    [tests] Renamed replay folder to replay-standalone

--- tests/testprogram/config_fortrantestgenerator_test_standalone.py
+++ tests/testprogram/config_fortrantestgenerator_test_standalone.py
@@ -7,5 +7,5 @@ TEMPLATE_DIR = FTG_DIR + '/templates/standalone'
 TEST_DIR = FTG_DIR + '/tests/testprogram'
 
 FCG_CONFIG_FILE = TEST_DIR + '/config_fortrancallgraph_test.py'
-TEST_SOURCE_DIR = TEST_DIR + '/replay'
+TEST_SOURCE_DIR = TEST_DIR + '/replay-standalone'
 TEST_DATA_BASE_DIR = TEST_DIR + '/data'
