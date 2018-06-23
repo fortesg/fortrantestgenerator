44083fa58a254d02fc7d383f6e3786c3f8205091
Christian Hovy <hovy@informatik.uni-hamburg.de>
2018-06-24 00:17:26 +0200

    Created template class

---
+++ templates.py
@@ -0,0 +1,40 @@
+from Cheetah.Template import Template
+from enum import Enum
+from assertions import assertType
+from templatenamespace import TemplatesNameSpace
+
+class FTGTemplate(Template):
+    
+    class Part(Enum):
+        CAPTURE_AFTER_LAST_LINE = 'captureAfterLastLine'
+        CAPTURE_BEFORE_CONTAINS = 'captureBeforeContains'
+        CAPTURE_AFTER_LAST_SPECIFICATION = 'captureAfterLastSpecification'
+        CAPTURE_BEFORE_END = 'captureBeforeEnd'
+        EXPORT = 'export'
+        REPLAY = 'replay'
+        
+    def __init__(self, part, namespace):
+        assertType(part, 'part', FTGTemplate.Part)
+        assertType(namespace, 'namespace', TemplatesNameSpace)
+        
+        super(FTGTemplate, self).__init__(part.value, searchList=[namespace])
+        
+    def captureAfterLastLine(self):
+        return ''
+    
+    def captureBeforeContains(self):
+        return ''
+    
+    def captureAfterLastSpecification(self):
+        return ''
+    
+    def captureBeforeEnd(self):
+        return ''
+    
+    def export(self):
+        return ''
+    
+    def replay(self):
+        return ''
+        
+    
