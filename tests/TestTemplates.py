45454a729579a7f1575a0d50787f48cdd30056ec
Christian Hovy <hovy@informatik.uni-hamburg.de>
2018-06-24 00:55:25 +0200

    [tests] DummyTemplateNameSpace for Template test

--- tests/TestTemplates.py
+++ tests/TestTemplates.py
@@ -7,18 +7,17 @@ import sys
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 FTG_DIR = TEST_DIR + '/..'
 sys.path.append(FTG_DIR)
-FCG_DIR = TEST_DIR + '/../../fortrancallgraph'
-sys.path.append(FCG_DIR)
 
 from templates import FTGTemplate
 from templatenamespace import TemplatesNameSpace
-from source import Subroutine, SubroutineFullName, Module, SourceFile
 
 class TestTemplate(unittest.TestCase):
     def setUp(self):
-        module = Module('mod', [], SourceFile('', isTestDummy=True), 0)
-        subroutine = Subroutine(SubroutineFullName('__mod_MOD_subr'), False, [], module)
-        self.namespace = TemplatesNameSpace(subroutine, [], [], '') 
+        
+        class DummyTemplatesNameSpace(TemplatesNameSpace):
+            def __init__(self):
+                pass
+        self.namespace = DummyTemplatesNameSpace() 
         
     def testPlain(self):
         self.assertEqual('', str(FTGTemplate(FTGTemplate.Part.CAPTURE_AFTER_LAST_LINE, self.namespace)))
