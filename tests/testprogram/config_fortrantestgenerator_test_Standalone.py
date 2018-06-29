e1dce2f5eb6e6a282071c7a1dc883b1a39471041
Christian Hovy <hovy@informatik.uni-hamburg.de>
2018-06-29 23:37:45 +0200

    [tests] Renamed what I have just missed

--- tests/testprogram/config_fortrantestgenerator_test_Standalone.py
+++ tests/testprogram/config_fortrantestgenerator_test_Standalone.py
@@ -7,5 +7,5 @@ TEMPLATE = FTG_DIR + '/templates/Standalone/Standalone.tmpl'
 TEST_DIR = FTG_DIR + '/tests/testprogram'
 
 FCG_CONFIG_FILE = TEST_DIR + '/config_fortrancallgraph_test.py'
-TEST_SOURCE_DIR = TEST_DIR + '/replay-standalone'
+TEST_SOURCE_DIR = TEST_DIR + '/replay-Standalone'
 TEST_DATA_BASE_DIR = TEST_DIR + '/data'
