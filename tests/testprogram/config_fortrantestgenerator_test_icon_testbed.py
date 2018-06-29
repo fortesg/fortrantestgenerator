da6f455b56222569baea60dcface9d8752bcbfd2
Christian Hovy <hovy@informatik.uni-hamburg.de>
2018-06-29 23:08:11 +0200

    Created new IconTestbed template

--- tests/testprogram/config_fortrantestgenerator_test_icon_testbed.py
+++ tests/testprogram/config_fortrantestgenerator_test_icon_testbed.py
@@ -2,7 +2,7 @@ import os
 
 FTG_DIR = os.path.dirname(os.path.realpath(__file__))
 FCG_DIR = FTG_DIR + '/../fortrancallgraph'
-TEMPLATE_DIR = FTG_DIR + '/templates/icon_testbed'
+TEMPLATE_DIR = FTG_DIR + '/templates/IconTestbed/IconTestbed.tmpl'
 
 TEST_DIR = FTG_DIR + '/tests/testprogram'
 
