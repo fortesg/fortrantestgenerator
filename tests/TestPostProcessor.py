0c67afd30c2a3da1f6324ae50ebb8baba69c219e
Christian Hovy <hovy@informatik.uni-hamburg.de>
2019-03-05 11:54:11 +0100

    Test for merge and no merge

--- tests/TestPostProcessor.py
+++ tests/TestPostProcessor.py
@@ -34,5 +34,65 @@ b = a
 '''
         self.assertEqual(expected, self.post.process(text))
     
+    def testNestingNoMerge(self):
+        text = '''
+        IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ### 
+          a = a + 1
+          IF (a == 1) THEN ! #### MERGE BEGIN abcde 123 ###
+            b = a
+          END IF ! #### MERGE BEGIN abcde 123 ###
+        END IF ! #### MERGE BEGIN abcde 123 ###
+        SWITCH TYPE (t) ! #### MERGE BEGIN abcde 123 ###
+          TYPE IS (test)
+            t%a = a
+        END SWITCH ! #### MERGE END abcde 123 ###
+        '''
+        expected = '''
+IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ###
+  a = a + 1
+  IF (a == 1) THEN ! #### MERGE BEGIN abcde 123 ###
+    b = a
+  END IF ! #### MERGE BEGIN abcde 123 ###
+END IF ! #### MERGE BEGIN abcde 123 ###
+SWITCH TYPE (t) ! #### MERGE BEGIN abcde 123 ###
+TYPE IS (test)
+  t%a = a
+END SWITCH ! #### MERGE END abcde 123 ###
+'''
+
+        self.assertEqual(expected, self.post.process(text))
+    
+    def testMergeIf(self):
+        text = '''
+        IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ### 
+          a = a + 1
+          IF (a == 1) THEN ! #### MERGE BEGIN abcde 123 ###
+            b = a
+          END IF ! #### MERGE BEGIN abcde 123 ###
+        END IF ! #### MERGE BEGIN abcde 123 ###
+        IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ### 
+          IF (a == 1) THEN ! #### MERGE BEGIN abcde 123 ###
+            b = a + 45
+          END IF ! #### MERGE BEGIN abcde 123 ###
+          IF (b == 2) THEN ! #### MERGE BEGIN abcde 123 ###
+            a = a * b
+          END IF ! #### MERGE BEGIN abcde 123 ###
+        END IF ! #### MERGE BEGIN abcde 123 ###
+        '''
+        expected = '''
+IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ### 
+  a = a + 1
+  IF (a == 1) THEN ! #### MERGE BEGIN abcde 123 ###
+    b = a
+    b = a + 45
+  END IF ! #### MERGE BEGIN abcde 123 ###
+  IF (b == 2) THEN ! #### MERGE BEGIN abcde 123 ###
+    a = a * b
+  END IF ! #### MERGE BEGIN abcde 123 ###
+END IF ! #### MERGE BEGIN abcde 123 ###
+'''
+
+        self.assertEqual(expected, self.post.process(text))
+    
 if __name__ == "__main__":
     unittest.main()
