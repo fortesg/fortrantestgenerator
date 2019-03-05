58ef7722eba33ef5ace9a5379550298beb6f4452
Christian Hovy <hovy@informatik.uni-hamburg.de>
2019-03-05 11:53:43 +0100

    Build CodeBlock nesting

--- postprocessor.py
+++ postprocessor.py
@@ -1,6 +1,7 @@
 import random
 import string
 import re
+from printout import printDebug
 
 class CodePostProcessor(object):
 
@@ -31,7 +32,8 @@ class CodePostProcessor(object):
         return "\n".join(lines)
     
     def __clearAndMerge(self, lines):
-        rootBlock = CodeBlock('')
+        rootBlock = CodeBlock('', None)
+        cBlock = rootBlock
         for line in lines:
             line = line.strip()
             if line == CodePostProcessor.CLEAR_LINE:
@@ -39,13 +41,26 @@ class CodePostProcessor(object):
             cLine = CodeLine(line)
             match = CodePostProcessor.MERGE_REGEX.match(line)
             if match is not None:
+                key = match.group('key')
                 if match.group('part') == 'BEGIN':
-                    pass
-                else:
-                    pass
+                    if cBlock.new() and cBlock.key == key:
+                        cBlock.begin(cLine)
+                    else:
+                        if cBlock.closed():
+                            cBlock = cBlock.parent
+                        newBlock = CodeBlock(key, cBlock)
+                        newBlock.begin(cLine)
+                        cBlock.content(newBlock)
+                        cBlock = newBlock
+                else: # END
+                    if cBlock.closed() and cBlock.key != key:
+                        cBlock = cBlock.parent
+                    cBlock.end(cLine)
+                    # TODO: What if cBLock.key != key?
             else:
-                pass
-            rootBlock.content.append(cLine)
+                if cBlock.closed():
+                    cBlock = cBlock.parent
+                cBlock.content(cLine)
             
         return rootBlock.render()
     
@@ -64,6 +79,13 @@ class CodePostProcessor(object):
         indent = baseIndent
         for line in lines:
             lineUpper = line.upper()
+            stringMask = self.__stringMask(line)
+            exclPos = line.find('!')
+            while exclPos >= 0 and stringMask[exclPos]:
+                exclPos = line.find('!', exclPos + 1)
+            if exclPos >= 0:
+                lineUpper = lineUpper[:exclPos].strip()
+                
             if lineUpper.startswith(endWords) or lineUpper.startswith(borderWords):
                 indent = max(baseIndent, indent - CodePostProcessor.INDENT_LENGTH)
             if lineUpper.startswith(doubleEndWords):
@@ -119,18 +141,40 @@ class CodePostProcessor(object):
         return mask
     
 class CodeBlock():
-    def __init__(self, key):
+    def __init__(self, key, parent):
         self.key = key
-        self.begin = []
-        self.content = []    
-        self.end = []
+        self.parent = parent
+        self.__begin = []
+        self.__content = []    
+        self.__end = []
+    
+    def begin(self, element):
+        self.__begin.append(element)
+    
+    def content(self, element):
+        self.__content.append(element)
+    
+    def end(self, element):
+        self.__end.append(element)
+    
+    def root(self):
+        return self.parent is None
+    
+    def new(self):
+        return not self.root() and not self.__content and not self.__end
+    
+    def closed(self):
+        return not self.root() and self.__end
         
     def render(self):
-        return [code.render() for code in self.begin + self.content + self.end]
+        return [code.render() for code in self.__begin + self.__content + self.__end]
         
 class CodeLine():
     def __init__(self, line):
-        self.line = line
+        self.__line = line
+    
+    def empty(self):
+        return not self.__line
         
     def render(self):
-        return self.line
+        return self.__line
