302dea9e7484eeb9cff485a39178eecae00da95b
Christian Hovy <hovy@informatik.uni-hamburg.de>
2019-03-05 15:25:03 +0100

    More tests for CodePostProcessor

--- tests/TestPostProcessor.py
+++ tests/TestPostProcessor.py
@@ -94,5 +94,110 @@ END IF ! #### MERGE END abcde 123 ####
         actual = self.post.process(text)
         self.assertEqual(expected, actual, '==== expected ====\n@' + expected + '@\n==== actual ====\n@' + actual + '@')
     
+    def testDontMergeIf(self):
+        text = '''
+        IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+          a = a + 1
+          IF (a == 1) THEN ! #### MERGE BEGIN abcde 456 ####
+            b = a
+          END IF ! #### MERGE END abcde 456 ####
+        END IF ! #### MERGE END abcde 123 ####
+
+
+        IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+          IF (a == 1) THEN ! #### MERGE BEGIN abcde 789 ####
+            b = a + 45
+          END IF ! #### MERGE END abcde 789 ####
+          IF (b == 2) THEN ! #### MERGE BEGIN abcde 456 ####
+            a = a * b
+          END IF ! #### MERGE END abcde 456 ####
+        END IF ! #### MERGE END abcde 123 ####
+        '''
+        expected = '''
+IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+  a = a + 1
+  IF (a == 1) THEN ! #### MERGE BEGIN abcde 456 ####
+    b = a
+  END IF ! #### MERGE END abcde 456 ####
+  IF (a == 1) THEN ! #### MERGE BEGIN abcde 789 ####
+    b = a + 45
+  END IF ! #### MERGE END abcde 789 ####
+  IF (b == 2) THEN ! #### MERGE BEGIN abcde 456 ####
+    a = a * b
+  END IF ! #### MERGE END abcde 456 ####
+END IF ! #### MERGE END abcde 123 ####
+'''
+        actual = self.post.process(text)
+        self.assertEqual(expected, actual, '==== expected ====\n@' + expected + '@\n==== actual ====\n@' + actual + '@')
+        
+    def testMergeSwitch(self):
+        text = '''
+        IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+          a = a + 1
+          IF (a == 1) THEN ! #### MERGE BEGIN abcde 456 ####
+            b = a
+          END IF ! #### MERGE END abcde 456 ####
+        END IF ! #### MERGE END abcde 123 ####
+        SWITCH TYPE (t) ! #### MERGE BEGIN abcde 789 ####
+          TYPE IS (test) ! #### MERGE BEGIN abcde 789 ####
+            t%a = a
+        END SWITCH ! #### MERGE END abcde 789 ####
+        SWITCH TYPE (t) ! #### MERGE BEGIN abcde 789 ####
+          TYPE IS (test) ! #### MERGE BEGIN abcde 789 ####
+            t%b = b
+        END SWITCH ! #### MERGE END abcde 789 ####
+        '''
+        expected = '''
+IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+  a = a + 1
+  IF (a == 1) THEN ! #### MERGE BEGIN abcde 456 ####
+    b = a
+  END IF ! #### MERGE END abcde 456 ####
+END IF ! #### MERGE END abcde 123 ####
+SWITCH TYPE (t) ! #### MERGE BEGIN abcde 789 ####
+TYPE IS (test) ! #### MERGE BEGIN abcde 789 ####
+  t%a = a
+  t%b = b
+END SWITCH ! #### MERGE END abcde 789 ####
+'''
+        actual = self.post.process(text)
+        self.assertEqual(expected, actual, '==== expected ====\n@' + expected + '@\n==== actual ====\n@' + actual + '@')
+            
+    def testNotSound(self):
+        text = '''
+        IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+          a = a + 1
+          IF (a == 1) THEN ! #### MERGE BEGIN abcde 456 ####
+            b = a
+          END IF ! #### MERGE END abcde 456 ####
+        END IF ! #### MERGE END abcde 012 ####
+        IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+          IF (a == 1) THEN ! #### MERGE BEGIN abcde 789 ####
+            b = a + 45
+          END IF ! #### MERGE END abcde 789 ####
+          IF (b == 2) THEN ! #### MERGE BEGIN abcde 456 ####
+            a = a * b
+          END IF ! #### MERGE END abcde 456 ####
+        END IF ! #### MERGE END abcde 123 ####
+        '''
+        expected = '''
+IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+  a = a + 1
+  IF (a == 1) THEN ! #### MERGE BEGIN abcde 456 ####
+    b = a
+  END IF ! #### MERGE END abcde 456 ####
+END IF ! #### MERGE END abcde 012 ####
+IF (a > b) THEN ! #### MERGE BEGIN abcde 123 ####
+  IF (a == 1) THEN ! #### MERGE BEGIN abcde 789 ####
+    b = a + 45
+  END IF ! #### MERGE END abcde 789 ####
+  IF (b == 2) THEN ! #### MERGE BEGIN abcde 456 ####
+    a = a * b
+  END IF ! #### MERGE END abcde 456 ####
+END IF ! #### MERGE END abcde 012 ####
+'''
+        actual = self.post.process(text)
+        self.assertEqual(expected, actual, '==== expected ====\n@' + expected + '@\n==== actual ====\n@' + actual + '@')
+        
 if __name__ == "__main__":
     unittest.main()
